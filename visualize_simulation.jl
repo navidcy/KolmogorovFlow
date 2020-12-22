771c2505075b434dad302ddbe559c99558de2585
Navid Constantinou <navidcy@gmail.com>
2020-12-22 14:28:46 +1100

    adds more diags are fixes ψ bug

--- visualize_simulation.jl
+++ visualize_simulation.jl
@@ -2,7 +2,7 @@ using JLD2
 using FourierFlows
 using Plots
 using Printf
-using LinearAlgebra: ldiv!
+using FFTW: irfft
 
 filename = "./data/kolmogorovflow.jld2"
 filename_diags = "./data/kolmogorovflow_diags.jld2"
@@ -83,11 +83,23 @@ function plot_output(x, y, ζ, ψ, t, k₀, ν, t_final)
                    # ylims = (0, 3),
                    )
 
+    p_diags3 = plot(3, # this means "a plot with two series"
+                   label = ["max(|∂²ψ/∂x²|)" "max(|∂²ψ/∂y²|)" "max(|∂²ψ/∂x∂y|)"],
+                  legend = :topright,
+               linewidth = 2,
+                   alpha = 0.7, 
+                  xlabel = "ν k₀² t",
+                   xlims = (0, 1.01 * ν * k₀^2 * t_final),
+                   # yscale = :log10,
+                   # ylims = (0, 3),
+                   )
+                   
     l = @layout [ Plots.grid(1, 2)
-                  c{0.25h}
-                  d{0.25h} ]
+                  c{0.22h}
+                  d{0.22h}
+                  e{0.22h} ]
              
-    p = plot(p_ζ, p_ψ, p_diags1, p_diags2, layout = l, size = (900, 1000))
+    p = plot(p_ζ, p_ψ, p_diags1, p_diags2, p_diags3, layout = l, size = (900, 1000))
 
     return p
 end
@@ -116,16 +128,13 @@ final_iteration = iterations[end]
 ζh_initial = file["snapshots/zetah/0"]
 t_final = file["snapshots/t/$final_iteration"]
 
-global ζ = zeros(nx, ny)
-global ψ = zeros(nx, ny)
-
 ζh = zeros(Complex{Float64}, (grid.nkr, grid.nl))
 ψh = zeros(Complex{Float64}, (grid.nkr, grid.nl))
 
 @. ζh = ζh_initial
-ldiv!(ζ, grid.rfftplan, ζh)
+ζ = irfft(ζh, grid.nx)
 @. ψh = @. -grid.invKrsq * ζh
-ldiv!(ψ, grid.rfftplan, ψh)
+ψ = irfft(ψh, grid.nx)
 
 energyh = @. 1 / 2 * grid.invKrsq * abs2(ζh_initial)
 energy_initial = 1 / (grid.Lx * grid.Ly) * FourierFlows.parsevalsum(energyh, grid)
@@ -139,9 +148,17 @@ global total_iterations = length(iterations)
 
 startwalltime = time()
 
+global ∂²ψ∂x² = zeros(grid.nx, grid.ny)
+global ∂²ψ∂y² = zeros(grid.nx, grid.ny)
+global ∂²ψ∂x∂y = zeros(grid.nx, grid.ny)
+
+global ∂²ψ∂x²h = zeros(Complex{Float64}, grid.nkr, grid.nl)
+global ∂²ψ∂y²h = zeros(Complex{Float64}, grid.nkr, grid.nl)
+global ∂²ψ∂x∂yh = zeros(Complex{Float64}, grid.nkr, grid.nl)
+
 anim = @animate for (i, iteration) in enumerate(iterations[1:end-1])
   
-  if i%25 == 0
+  if i%50 == 0
     estimated_remaining_walltime = (time()-startwalltime)/60 / i * (total_iterations - i)
     log = @sprintf("estimated remaining walltime: %.2f min", estimated_remaining_walltime)
     println(log)
@@ -151,21 +168,30 @@ anim = @animate for (i, iteration) in enumerate(iterations[1:end-1])
   local tν = ν * k₀^2 * t
   
   local ζh = file["snapshots/zetah/$iteration"] 
-  ldiv!(ζ, grid.rfftplan, ζh)
+  local ζ = irfft(ζh, grid.nx)
   
   @. ψh = -grid.invKrsq * ζh
-  ldiv!(ψ, grid.rfftplan, ψh)
-  
+    
   hypeh = @. (grid.kr^2 - grid.l^2) * ψh
-  hype = zeros(grid.nx, grid.ny)
-  ldiv!(hype, grid.rfftplan, hypeh)  
-  hype00 = hype[Int(grid.nx/2), Int(grid.ny/2)]
+  hype = irfft(hypeh, grid.nx)
+  hype_00 = hype[Int(grid.nx/2), Int(grid.ny/2)]
 
-  crossh = @. grid.kr * grid.l * ψh
-  cross = zeros(grid.nx, grid.ny)
-  ldiv!(cross, grid.rfftplan, crossh)
-  cross00 = cross[Int(grid.nx/2), Int(grid.ny/2)]
+  @. ∂²ψ∂x²h = grid.kr^2 * ψh
+  @. ∂²ψ∂y²h = grid.l * ψh
+  @. ∂²ψ∂x∂yh = grid.kr * grid.l * ψh
   
+  local ∂²ψ∂x² = irfft(∂²ψ∂x²h, grid.nx)  
+  local ∂²ψ∂y² = irfft(∂²ψ∂y²h, grid.nx)  
+  local ∂²ψ∂x∂y = irfft(∂²ψ∂x∂yh, grid.nx)
+    
+  ∂²ψ∂x∂y_00 = ∂²ψ∂x∂y[Int(grid.nx/2), Int(grid.ny/2)]
+  
+  max∂²ψ∂x² = maximum(abs.(∂²ψ∂x²))
+  max∂²ψ∂y² = maximum(abs.(∂²ψ∂y²))
+  max∂²ψ∂x∂y = maximum(abs.(∂²ψ∂x∂y))
+  
+  local ψ = irfft(ψh, grid.nx)
+
   p[1][1][:z] = ζ'
   p[1][:title] = "vorticity, tν = " * @sprintf("%.4f", tν)
   p[2][1][:z] = ψ'
@@ -185,8 +211,11 @@ anim = @animate for (i, iteration) in enumerate(iterations[1:end-1])
   push!(p[3][2], tν_diags, ΔΖ₂)
   push!(p[3][3], tν_diags, ΔΖ₄)
   push!(p[3][4], tν_diags, ΔP)
-  push!(p[4][1], tν, cross00)
-  push!(p[4][2], tν, hype00)
+  push!(p[4][1], tν, ∂²ψ∂x∂y_00)
+  push!(p[4][2], tν, hype_00)
+  push!(p[5][1], tν, max∂²ψ∂x²)
+  push!(p[5][2], tν, max∂²ψ∂y²)
+  push!(p[5][3], tν, max∂²ψ∂x∂y)
 end
 
 gif(anim, moviegif_filename, fps=14)
