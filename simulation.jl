1cfe358f8df9430104f2798c2e68a5f63bb42f88
Navid Constantinou <navidcy@gmail.com>
2020-12-24 09:20:35 +1100

    fix bug in vorticityL4()

--- simulation.jl
+++ simulation.jl
@@ -1,26 +1,11 @@
 # ## Problem setup
 # We initialize a `Problem` by providing a set of keyword arguments. The
 # `stepper` keyword defines the time-stepper to be used.
-prob = TwoDNavierStokes.Problem(dev; nx=nx, Lx=Lx, ny=ny, Ly=Ly, ν=ν, dt=dt, stepper="ETDRK4")
+prob = TwoDNavierStokes.Problem(dev; nx=nx, Lx=Lx, ny=ny, Ly=Ly, ν=ν, dt=dt, calcF=calcF!, stepper="ETDRK4")
 
 # Next we define some shortcuts for convenience.
 sol, cl, vs, gr = prob.sol, prob.clock, prob.vars, prob.grid
 x, y = gr.x, gr.y
-
-
-# ## Setting initial conditions
-
-# Our initial condition closely tries to reproduce the initial condition used
-# in the paper by McWilliams (_JFM_, 1984)
-seed!(1234)
-E₀ = 0.00025 # energy of sin(y) is 0.25
-
-# ζ_perturbation = peakedisotropicspectrum(gr, k₀, E₀, mask=prob.timestepper.filter)
-# ζ_perturbation = peakedisotropicspectrum(gr, k₀, E₀)
-#ζ₀ = @. sin(Y) + ζ_perturbation
-
-X, Y = gridpoints(gr)
-ζ₀ = @. initial_vorticity(X, Y) 
 
 TwoDNavierStokes.set_zeta!(prob, ζ₀)
 
@@ -41,7 +26,7 @@ Returns the domain-averaged enstrophy, ∫ ζ⁴ dxdy / (Lx Ly), for the solutio
   sol, vars, grid = prob.sol, prob.vars, prob.grid
   @. vars.zetah = sol
   ldiv!(vars.zeta, grid.rfftplan, vars.zetah)
-  return sum(@. vs.zeta^4) * gr.dx * gr.dy / (gr.Lx * gr.Ly)
+  return sum(@. vars.zeta^4) * grid.dx * grid.dy / (grid.Lx * grid.Ly)
 end
 
 """
@@ -111,8 +96,8 @@ for j=0:Int(round(nsteps/nsubs))-1
     cfl = cl.dt * maximum([maximum(vs.u) / gr.dx, maximum(vs.v) / gr.dy])
     
     estimated_remaining_walltime = (time()-startwalltime)/60 / cl.step * (nsteps-cl.step)
-    log = @sprintf("step: %04d, t: %d, tν : %.4f, ΔE: %.4f, ΔZL₂: %.4f, ΔZL₄: %.4f, P: %.4f, cfl: %.4f, walltime: %.2f min, estimated remaining walltime: %.2f min",
-      cl.step, cl.t, ν*k₀^2*cl.t, E.data[E.i]/E.data[1], Z2.data[Z2.i]/Z2.data[1], Z4.data[Z4.i]/Z4.data[1], P.data[P.i]/P.data[1], cfl, (time()-startwalltime)/60, estimated_remaining_walltime)
+    log = @sprintf("step: %04d, t: %d, tν : %.4f, E: %.3e, Zₗ₂: %.3e, Zₗ₄: %.3e, P: %.3e, cfl: %.4f, walltime: %.2f min, estimated remaining walltime: %.2f min",
+      cl.step, cl.t, ν*k₀^2*cl.t, E.data[E.i], Z2.data[Z2.i], Z4.data[Z4.i], P.data[P.i], cfl, (time()-startwalltime)/60, estimated_remaining_walltime)
     println(log)
   end  
 
