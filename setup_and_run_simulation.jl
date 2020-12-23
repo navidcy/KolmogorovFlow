67ad6d23a590065bd98d399135900628bc2ea8fc
Navid Constantinou <navidcy@gmail.com>
2020-12-24 09:20:58 +1100

    adds forcing + perturbation of initial data

--- setup_and_run_simulation.jl
+++ setup_and_run_simulation.jl
@@ -18,27 +18,48 @@ import GeophysicalFlows: peakedisotropicspectrum
 
 dev = CPU()     # Device (CPU/GPU)
 
-
 # ## Numerical, domain, and simulation parameters
 #
 # First, we pick some numerical and physical parameters for our model.
 
-α = 0.9                       # aspect ratio parameter α = Ly / Lx
+α = 1.2                       # aspect ratio parameter α = Ly / Lx
 
-ny, Ly  = 256, 2π             # grid resolution and domain length
-nx, Lx  = 256, Ly / α         # grid resolution and domain length
+ny, Ly  = 128, 2π             # grid resolution and domain length
+nx, Lx  = 126, Ly / α
 
 ## Then we pick the time-stepper parameters
-    dt = 1e-2  # timestep
- nsubs = 50    # number of steps between each plot
+    dt = 1e-3  # timestep
+ nsubs = 1    # number of steps between each plot
  
-  ν = 1e-4
+  ν = 2e-1
  k₀ = 1
- tfinal = 0.005 / (ν * k₀^2)
+ tfinal = 0.2 / (ν * k₀^2)
  nsteps = Int(round(tfinal / dt))
+
+ 
+ grid = TwoDGrid(dev, nx, Lx, ny, Ly)
+ x, y = gridpoints(grid)
+
+ γ = 1.0
+ forcing = @. γ * cos(y)
+ forcingh = rfft(forcing)
+
+ # Function that constructs the vorticity forcing in Fourier space
+ function calcF!(Fh, sol, t, clock, vars, params, grid)
+   @. Fh = forcingh
+   return nothing
+ end
 
 
 # Initial condition (as function)
 initial_vorticity(x, y) = sin(α * x) * sin(y) + 0.05 * (cos(α * 8x) + cos(8y))
+initial_vorticity(x, y) = γ / ν * cos(y)
+
+# Setting initial conditions
+seed!(1234)
+
+ζ_perturbation = 1 * randn(nx, ny)
+
+ζ₀ = @. initial_vorticity(x, y) + ζ_perturbation
 
 include("simulation.jl")
