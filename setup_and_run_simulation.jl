f603906a51001e451caf0babbce9d7fb87073708
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-01-24 11:00:52 +1100

    adds stochastic forcing

--- setup_and_run_simulation.jl
+++ setup_and_run_simulation.jl
@@ -27,6 +27,9 @@ dev = CPU()     # Device (CPU/GPU)
 α = 0.3                       # aspect ratio parameter α = Ly / Lx
 γ = 1.0                       # forcing amplitude
 
+add_stochastic_forcing = true
+ε = 0.1                       # energy input rate by the forcing
+
 critical_ν(α, γ) = sqrt((γ * sqrt(1 - α^2)) / (sqrt(2) * (1 + α^2)))
 
 ny, Ly  = 64, 2π             # grid resolution and domain length
@@ -48,12 +51,32 @@ nx, Lx  = 128, Ly / α
  forcing = @. γ * cos(y)
  forcingh = rfft(forcing)
 
- # Function that constructs the vorticity forcing in Fourier space
- function calcF!(Fh, sol, t, clock, vars, params, grid)
-   @. Fh = forcingh
-   return nothing
- end
+if add_stochastic_forcing == true
+  K = @. sqrt(grid.Krsq)             # a 2D array with the total wavenumber
 
+  forcing_spectrum = 0*grid.Krsq
+
+  forcing_spectrum[grid.Krsq .== 1.0] .= 1
+  forcing_spectrum[grid.Krsq .== 4.0] .= 1
+  
+  forcing_spectrum[grid.Krsq .== 0] .= 0 # ensure forcing has zero domain-average
+
+  ε0 = FourierFlows.parsevalsum(forcing_spectrum .* grid.invKrsq / 2, grid) / (grid.Lx * grid.Ly)
+  @. forcing_spectrum *= ε/ε0        # normalize forcing to inject energy at rate ε
+
+  random_uniform = dev==CPU() ? rand : CUDA.rand
+
+  function calcF!(Fh, sol, t, clock, vars, params, grid)
+    Fh .= forcingh .+ sqrt.(forcing_spectrum) .* exp.(2π * im * random_uniform(eltype(grid), size(sol))) ./ sqrt(clock.dt)
+    return nothing
+  end
+else
+  # Function that constructs the deterministic vorticity forcing in Fourier space
+  function calcF!(Fh, sol, t, clock, vars, params, grid)
+    @. Fh = forcingh
+    return nothing
+  end
+end
 
 # Initial condition (as function)
 initial_vorticity(x, y) = sin(α * x) * sin(y) + 0.05 * (cos(α * 8x) + cos(8y))
